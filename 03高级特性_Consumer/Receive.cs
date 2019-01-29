a1c543f81fd717bc27c0fdb16b5520588da51938
Sammo <sammo.wong@foxmail.com>
2019-01-29 11:23:31 +0800

    添加高级特性示例

---
+++ 03高级特性_Consumer/Receive.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace _03高级特性_Consumer
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var queue = "高级特性";
+                    channel.QueueDeclare(queue, true, false, false, null);//定义一个支持持久化的消息队列
+                    channel.BasicQos(0, 1, false);//在一个消费者还在处理消息且没响应消息之前，不要给他分发新的消息，而是将这条新的消息发送给下一个不那么忙碌的消费者
+
+                    Console.WriteLine("准备接收消息：");
+                    var consumer = new EventingBasicConsumer(channel);//构建消费者实例
+                    consumer.Received += (sender, e) =>
+                    {
+                        var body = e.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        SimulationTask(message);
+                        channel.BasicAck(e.DeliveryTag, false);//消息确认信号（手动消息确认）
+                    };
+
+                    channel.BasicConsume(queue, false, consumer);
+
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 模拟消息任务的处理过程
+        /// </summary>
+        /// <param name="message">消息</param>
+        private static void SimulationTask(string message)
+        {
+            Console.WriteLine("接收的消息： {0}", message);
+            int dots = message.Split('.').Length - 1;
+            Thread.Sleep(dots * 1000);
+            Console.WriteLine("接收的消息处理完成，现在时间为{0}！", DateTime.Now);
+        }
+    }
+}
---
+++ 03高级特性_Publisher/Send.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace _03高级特性_Publisher
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    var queue = "高级特性";
+                    channel.QueueDeclare(queue, true, false, false, null);//定义一个支持持久化的消息队列
+                    var properties = channel.CreateBasicProperties();
+                    properties.DeliveryMode = 2;//1:不持久化，2：持久化
+                    Console.WriteLine("请注意：演示耗时较长的消息时，可通过发送带有‘.’的内容去模拟，每个‘.’加1秒！");
+                    while (true)
+                    {
+                        Console.WriteLine("请输入要发送的信息：");
+                        var message = Console.ReadLine();
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish("", queue, properties, body);
+                        Console.WriteLine("已发送的消息： {0}", message);
+                    }
+                }
+            }
+        }
+    }
+}
